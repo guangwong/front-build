598a6022ac01d1d3222b9c817e2917b8a0df5b80
maxbbn <qipbbn@gmail.com>
2012-04-26 18:30:26 +0800

    update

--- bin/fb.js
+++ bin/fb.js
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
-var fs = require('fs');
-var argv = require('optimist')
-	.demand(1)
-	.usage("$0 init \n$0 add {pagename}")
-	.argv;
+var fs = require('fs'),
+	argv = require('optimist')
+		.demand(1)
+		.usage("$0 init \n$0 add {pagename}")
+		.argv,
+	cwd = process.cwd(),
+	FrontBuild = require('../lib/'),
+	getRoot = require('../lib/getRoot');
 
-var FrontBuild = require('../lib/');
+getRoot(function (err, root) {
+	process.chdir(cwd);
+	if (err) {
+		console.log(err);
+		process.exit(1);
+	}
 
-switch(argv._[0]) {
-	case 'init':
-		FrontBuild.init(process.cwd());
-		break;
-	case 'add':
-		FrontBuild.add(argv);
-		break;
-}
+	if (argv._[0] === 'init' && root) {
+		console.log('已经是一个工作目录了');
+		process.exit(2);
+	}
+
+	switch(argv._[0]) {
+		case 'init':
+			FrontBuild.init(cwd, function(err, doc){
+				console.log('初始化完毕');
+				process.exit(0);
+			});
+			break;
+		case 'add':
+			FrontBuild.add(argv, root);
+			break;
+		case 'version':
+			FrontBuild.version(argv, root);
+			break;
+	}
+});
