36a957493a731e0b8dafd38e4cf114f3d6b7edaf
maxbbn <qipbbn@gmail.com>
2012-06-11 17:49:40 +0800

    update test

--- test/page.test.js
+++ test/page.test.js
@@ -6,14 +6,16 @@ var fu = require('../lib/fileutil');
 
 
 describe('page build test', function(){
-    var rootDir = path.resolve('sample-project', 'page1');
+    var pageName = 'page1';
+    var rootDir = path.resolve('sample-project', pageName);
     var version = '1.0';
     var timestamp = '20120505';
     var thepage;
 
     before(function (done) {
         thepage = new Page({
-            rootDir: rootDir
+            rootDir: rootDir,
+            name: pageName
         });
 
         thepage.setVersion(
@@ -25,13 +27,21 @@ describe('page build test', function(){
                 thepage.build(timestamp, done);
             }
         );
+
     });
 
     after(function (done) {
-        // fu.rmTreeSync(path.resolve(rootDir, timestamp));
+        fu.rmTreeSync(path.resolve(rootDir, timestamp));
         done();
     });
 
+    it('should create a Page right object', function () {
+        thepage.name.should.be.eql(pageName);
+        thepage.rootDir.should.eql(rootDir);
+        thepage.config.should.be.a('object');
+        should.exist(thepage.srcDir);
+        should.exist(thepage.destDir);
+    });
 
     it('should create timestamp directory under rootDir', function(done){
         path.exists(path.resolve(rootDir, timestamp), function (exist) {
@@ -55,6 +65,64 @@ describe('page build test', function(){
             content.should.include('mods:mod1.js');
             content.should.include('mods:mod2.js');
             done();
+        });
+    });
+
+    it('should build less', function(done) {
+        var buildLessFile = path.resolve(rootDir, timestamp, 'core/index.less.css');
+
+        fs.readFile(buildLessFile, 'utf8', function(err, data) {
+            if (err) {
+                return done(err);
+            }
+            data.should.include('#a.less');
+            done();
+        });
+        
+    });
+
+    it('should compress css to -min.css', function(done) {
+        var minLessCss = path.resolve(rootDir, timestamp, 'core/index.less-min.css');
+        var minIndexCss = path.resolve(rootDir, timestamp, 'core/index-min.css')
+
+        fs.readFile(minLessCss, 'utf8', function(err, data) {
+            if (err) {
+                return done(err);
+            }
+            data.should.include('#a.less');
+
+            fs.readFile(minIndexCss, 'utf8', function(err, data) {
+                if (err) {
+                    return done(err);
+                }
+                data.should.include('#a.css');
+                data.should.include('#b.css');
+                done();
+            });
+        });
+        
+    });
+
+    it('should compress js to -min.js', function(done) {
+        var minIndexJS = path.resolve(rootDir, timestamp, 'core/index-min.js');
+        var minConcatJS = path.resolve(rootDir, timestamp, 'core/concat-min.js');
+
+        fs.readFile(minIndexJS, 'utf8', function (err, data) {
+            if (err) {
+                return done(err);
+            }
+            data.should.include('mods:mod1');
+            data.should.include('mods:mod2');
+            data.should.include('mods:submod1');
+            fs.readFile(minConcatJS, function (err, data) {
+                if (err) {
+                    return done(err);
+                }
+                data.should.include('mods:mod1');
+                data.should.include('mods:mod2');
+                data.should.include('mods:submod1');
+                done();
+            })
         });
     });
 });
@@ -119,62 +187,44 @@ describe('page add version test', function(){
         });
     });
 
-    it('should build less', function(done) {
-        var buildLessFile = path.resolve(rootDir, timestamp, 'core/index.less.css');
+    
 
-        fs.readFile(buildLessFile, 'utf8', function(err, data) {
+});
+
+
+describe('page addVersion test', function(){
+
+    var pageName = 'page1';
+    var rootDir = path.resolve('sample-project', pageName);
+    var version = '100.002';
+    var thepage;
+
+    before(function (done) {
+        thepage = new Page({
+            rootDir: rootDir,
+            name: pageName
+        });
+
+        thepage.addVersion(version, done);
+
+    });
+
+    after(function (done) {
+        fu.rmTreeSync(path.resolve(rootDir, version));
+        done();
+    });
+
+    it('should create a fb.page.json in rootDir', function (done) {
+        var json_path = path.resolve(rootDir, version, 'fb.page.json');
+        fu.readJSON(json_path, function (err, json) {
             if (err) {
                 return done(err);
             }
-            data.should.include('#a.less');
-            done();
-        });
-        
-    });
-
-    it('should compress css to -min.css', function(done) {
-        var minLessCss = path.resolve(rootDir, timestamp, 'core/index.less-min.css');
-        var minIndexCss = path.resolve(rootDir, timestamp, 'core/index-min.css')
-
-        fs.readFile(minLessCss, 'utf8', function(err, data) {
-            if (err) {
-                return done(err);
-            }
-            data.should.include('#a.less');
-
-            fs.readFile(minIndexCss, 'utf8', function(err, data) {
-                if (err) {
-                    return done(err);
-                }
-                data.should.include('#a.css');
-                data.should.include('#b.css');
-                done();
-            });
-        });
-        
-    });
-
-    it('should compress js to -min.js', function(done) {
-        var minIndexJS = path.resolve(rootDir, timestamp, 'core/index-min.js');
-        var minConcatJS = path.resolve(rootDir, timestamp, 'core/concat-min.js');
-
-        fs.readFile(minIndexJS, 'utf8', function (err, data) {
-            if (err) {
-                return done(err);
-            }
-            data.should.include('mods:mod1');
-            data.should.include('mods:mod2');
-            data.should.include('mods:submod1');
-            fs.readFile(minConcatJS, function (err, data) {
-                if (err) {
-                    return done(err);
-                }
-                data.should.include('mods:mod1');
-                data.should.include('mods:mod2');
-                data.should.include('mods:submod1');
-                done();
-            })
+            json.should.be.ok;
+            json.should.be.a('object');
+            should.exist(json.outputCharset);
+            should.exist(json.inputCharset);
+            done(null);
         });
     });
-
 });
