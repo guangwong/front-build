f8c83c357ff1a1c5acd43438874cf36cd3808e5e
maxbbn <qipbbn@gmail.com>
2012-07-18 10:35:04 +0800

    update test for watchdir

--- test/watchdir.test.js
+++ test/watchdir.test.js
@@ -29,13 +29,13 @@ describe('watchDir', function () {
 			}
 			watcher = watchDir(rootDir);
 			watcher.once('init', done);
-			watcher.on('rename', function (file) {
-				console.log('rename: %s', file.filename);
-			});
+		// 	watcher.on('rename', function (file) {
+		// 		console.log('rename: %s', file.filename);
+		// 	});
 
-			wather.on('update', function () {
-				console.log('update: %s', file.filename);
-			});
+		// 	wather.on('update', function () {
+		// 		console.log('update: %s', file.filename);
+		// 	});
 		});
 
 	});
@@ -48,6 +48,7 @@ describe('watchDir', function () {
 	it('should emit rename when file added', function (done) {
 		console.log('----add  file filePath');
 		watcher.on('rename', function (file) {
+			console.log('rename: %s', file.filename);
 			if (rootDir === file.filename) {
 				watcher.removeAllListeners('rename');
 				done();
@@ -62,7 +63,8 @@ describe('watchDir', function () {
 	it('should emit change when file updated', function (done) {
 		console.log('----update file filePath');
 		watcher.on('change', function (file) {
-			if (file.filename == filePath) {
+			console.log('change: %s', file.filename);
+			if (file.filename === filePath) {
 				watcher.removeAllListeners('change');
 				done();
 			}
@@ -74,8 +76,8 @@ describe('watchDir', function () {
 	it('should emit events when direcotory added or file added to subDire', function (done) {
 		console.log('----add subDir');
 		watcher.on('rename', function (file) {
-			console.log('reanme: ', file.filename);
-			if (file.filename == rootDir) {
+			console.log('reanme: %s', file.filename);
+			if (file.filename === rootDir) {
 				watcher.removeAllListeners('rename');
 				done();
 			}
@@ -86,8 +88,8 @@ describe('watchDir', function () {
 	it('should emit events when direcotory added or file added to subDire', function (done) {
 		console.log('----add subFile.js');
 		watcher.on('rename', function (file) {
-			console.log('reanme: ', file.filename);
-			if (file.filename == subDir) {
+			console.log('reanme: %s', file.filename);
+			if (file.filename === subDir) {
 				watcher.removeAllListeners('rename');
 				done();
 			}
@@ -100,8 +102,8 @@ describe('watchDir', function () {
 		console.log('----update subFile.js');
 
 		watcher.on('change', function (file) {
-			console.log('change: ', file.filename);
-			if (file.filename == subFile) {
+			console.log('change: %s', file.filename);
+			if (file.filename === subFile) {
 				watcher.removeAllListeners('change');
 				done();
 			}
