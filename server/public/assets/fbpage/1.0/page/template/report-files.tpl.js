157176c64c4fd6ea57980faf676b2421c47b011c
maxbbn <qipbbn@gmail.com>
2012-08-07 22:38:16 +0800

    ADD Templates

---
+++ server/public/assets/fbpage/1.0/page/template/report-files.tpl.js
@@ -0,0 +1,3 @@
+KISSY.add(function(){
+    return {"html":"<h4>处理文件列表:</h4>\n{{#if !files.length}}\n    <div>\n        没有文件\n    </div>\n{{#else}}\n    <ul class=\"plugin-file-list\">\n        {{#each files as file}}\n            <li>\n                <i class=\"icon-file\"></i> {{file}}\n            </li>\n        {{/each}}\n    </ul>\n{{/if}}\n"};
+});
