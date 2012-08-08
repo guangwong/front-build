03a30b4e51f56ead957d86b8d6b6e84d65556f1d
maxbbn <qipbbn@gmail.com>
2012-08-08 21:52:50 +0800

    kissy template update

---
+++ server/public/assets/fbpage/1.0/page/template/report-wrap-tpl.js
@@ -0,0 +1,3 @@
+KISSY.add(function(){
+    return {"html":"<div class=\"report\" style='display:none'>\n    <div class=\"report-hd\">{{fb}}</div>\n    <div class=\"report-bd\">{{plugins}}</div>\n</div>"};
+});
