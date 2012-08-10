4d17607e01f068ae215a8883678e79a0bd3a9fd0
maxbbn <qipbbn@gmail.com>
2012-08-10 11:24:47 +0800

    Logo 可以点击

--- server/public/assets/fbindex/1.0/page/template/app-history-tpl.js
+++ server/public/assets/fbindex/1.0/page/template/app-history-tpl.js
@@ -1,3 +1,3 @@
 KISSY.add(function(){
-    return {"html":"{{#each his as item}}\n<div>\n    <a href=\"/app?root={{item}}\"> {{item}} </a>\n</div>\n{{/each}}\n"};
+    return {"html":"<h3>历史记录：</h3>\n{{#each his as item}}\n<div class='history-item'>\n    <a href=\"/app?root={{item}}\"> {{item}} </a>\n</div>\n{{/each}}\n"};
 });
--- server/public/assets/fbindex/20120722/page/index.js
+++ server/public/assets/fbindex/20120722/page/index.js
@@ -58,7 +58,7 @@ KISSY.add('utils/app-history',function (S) {
         }
     }
 });KISSY.add('page/template/app-history-tpl',function(){
-    return {"html":"{{#each his as item}}\n<div>\n    <a href=\"/app?root={{item}}\"> {{item}} </a>\n</div>\n{{/each}}\n"};
+    return {"html":"<h3>历史记录：</h3>\n{{#each his as item}}\n<div class='history-item'>\n    <a href=\"/app?root={{item}}\"> {{item}} </a>\n</div>\n{{/each}}\n"};
 });KISSY.add('page/index',function (S, Template, appHistory, app_history_tpl) {
     var $ = S.all;
     if (appHistory) {
