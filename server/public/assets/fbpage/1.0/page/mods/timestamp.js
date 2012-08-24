4590dca4ef9023624acbfcd1235ad703cb0561cd
maxbbn <qipbbn@gmail.com>
2012-08-24 18:45:36 +0800

    add effect

--- server/public/assets/fbpage/1.0/page/mods/timestamp.js
+++ server/public/assets/fbpage/1.0/page/mods/timestamp.js
@@ -5,7 +5,25 @@ KISSY.add(function (S) {
     S.ready(function () {
         $('body').delegate('click', '.build-timestamp', function (ev) {
             var $et = $(ev.target);
-            $('.timestamp-input').val($et.html());
+            var $input = $('.timestamp-input');
+            var $clone = $et.clone(true);
+            var offsetFrom = $et.offset();
+            var offsetTo = $input.offset();
+            $clone.appendTo('body');
+            $clone
+                .css('position', 'absolute')
+                .css('left', offsetFrom.left)
+                .css('top', offsetFrom.top)
+                .show()
+                .animate({
+                    'left': offsetTo.left,
+                    'top': offsetTo.top
+                }, .2, 'easeNone', function () {
+                    $input.val($et.html());
+                    setTimeout(function () {
+                        $clone.remove();
+                    }, 0);
+                });
         })
     })
 });
