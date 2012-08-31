7aa90e3a31c63f2929c0ba1eaa7db4eb155a1e35
maxbbn <qipbbn@gmail.com>
2012-08-31 14:07:58 +0800

    rm test script

--- watcht.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-fs.watch('t', function (ev, filename){
-    console.log(ev, filename);
-});
-fs.watch('t/file', function (ev, filename){
-    console.log(ev, filename);
-});
