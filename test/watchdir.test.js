ce8214e2100e6fc93c3c42bd0d5cf202afbf805e
maxbbn <qipbbn@gmail.com>
2012-09-07 16:10:26 +0800

    update watch dir test

--- test/watchdir.test.js
+++ test/watchdir.test.js
@@ -10,19 +10,6 @@ describe('WatchDir', function () {
 	var subDir = path.resolve(rootDir, 'subDir');
 	var subFile = path.resolve(subDir, 'subFile.js');
 	var watcher;
-	var eventList = [];
-	var expectedEvent = [];
-	var config = {
-			exludes: [
-				'.svn',
-				'.git'
-			],
-			includes: [
-				'*.js',
-				'*.css',
-				'*.less'
-			]
-		};
 
 	before(function (done) {
 		fs.mkdir(rootDir, function (err) {
@@ -31,14 +18,6 @@ describe('WatchDir', function () {
 			}
 			watcher = new WatchDir(rootDir);
 
-			watcher.on('change', function (ev) {
-				eventList.push(['change',ev.path]);
-			});
-
-			watcher.on('rename', function (ev) {
-				eventList.push(['rename',ev.path]);
-			});
-
 			watcher.once('init', done);
 		});
 
@@ -46,61 +25,52 @@ describe('WatchDir', function () {
 
 	after(function (done){
 		watcher.stop();
-		console.log('eventList:')
-
-		var out = eventList.map(function (ev) {
-			return ev.join(': ');
-		}).join('\n');
-
-		var eventTypes = eventList.map(function (ev) {
-			return ev[0];
-		});
-
-		var expectedTypes = expectedEvent.map(function (ev) {
-			return ev[0];
-		});
-		eventTypes.should.eql(expectedTypes);
-
-		console.log(out);
 		fu.rmTree(rootDir, done);
 	});
 
 	it('should emit rename when file added', function (done) {
-		expectedEvent.push(['rename'])
+		// expectedEvent.push(['rename'])
+		watcher.once('rename', function () {
+			setTimeout(done, 100);
+		});
 		fs.writeFile(rootFile, 'hello');
-		setTimeout(done, 200);
 
 	});
 
 	it('should emit change when file updated', function (done) {
-		expectedEvent.push(['change'])
+		watcher.once('change', function () {
+			setTimeout(done, 100);
+		});
 		fs.writeFile(rootFile, 'hello2');
-		setTimeout(done, 200);
 
 	});
 
 	it('should emit events when direcotory added or file added to subDire', function (done) {
-		expectedEvent.push(['rename'])
+		watcher.once('rename', function () {
+			setTimeout(done, 100);
+		});
 		fs.mkdir(subDir);
-		setTimeout(done, 200);
 	});
 
 	it('should emit events when direcotory added or file added to subDire', function (done) {
-		expectedEvent.push(['rename']);
+		watcher.once('rename', function () {
+			setTimeout(done, 100);
+		});
 		fs.writeFile(subFile, 'subFile.js');
-		setTimeout(done, 200);
 	});
 
 	it('should emit events when direcotory added or file added to subDire', function (done) {
-		expectedEvent.push(['change']);
+		watcher.once('change', function () {
+			setTimeout(done, 100);
+		});
 		fs.writeFile(subFile, 'subFile.js update');
-		setTimeout(done, 200);
 	});
 
 	it('should emit events when subfile removed', function (done) {
-		expectedEvent.push(['rename']);
+		watcher.once('rename', function () {
+			setTimeout(done, 100);
+		});
 		fs.unlink(subFile);
-		setTimeout(done, 200);
 	});
 
 });
